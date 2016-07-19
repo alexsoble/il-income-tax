96b2e0abfd8777ecdfc92b31fa29ff076839caac
Alex Soble <asoble@gmail.com>
2016-07-19 06:23:26 -0500

    Pull out property taxes chart into its own file

---
+++ property_taxes_chart.js
@@ -0,0 +1,62 @@
+$(function () {
+
+  $('#container').highcharts({
+    chart: {
+      type: 'column'
+    },
+    title: {
+      text: 'Median real estate property taxes paid for housing units with mortgages (2013)'
+    },
+    subtitle: {
+      text: 'Source: city-data.com'
+    },
+    xAxis: {
+      categories: [
+        'Chicago, IL',
+        'Harvey, IL',
+        'East St. Louis, IL',
+      ],
+      title: {
+        text: null
+      }
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: 'Dollars'
+      },
+      labels: {
+        overflow: 'justify',
+        format: "${value}"
+      }
+    },
+    plotOptions: {
+      bar: {
+        dataLabels: {
+          enabled: false
+        }
+      },
+      series: {
+        allowPointSelect: false
+      }
+    },
+    legend: {},
+    tooltip: {
+      formatter: function() {
+        return '<b>Median property taxes:</b> $' + this.y;
+      }
+    },
+    credits: { enabled: false },
+    series: [
+      {
+        name: 'Median property taxes',
+        color: '#E0CA3C',
+        data: [
+          3645,
+          2379,
+          1134,
+        ]
+      }
+    ]
+  });
+});
